45a31f192795b235aa28a2e322d027ad48402a40
Somy-John <63829204+Somy-John@users.noreply.github.com>
2021-05-01 12:15:25 +0900

    Update manager.h

--- manager.h
+++ manager.h
@@ -1,3 +1,11 @@
-int attend (int num){
-  
-}
+
+void printList (int num, People p);      //명부 출력
+int attend (int num, People p);          //출석
+int goHome (int num, People p);          //퇴근
+int night (int num, People p);           //야근
+int earlyLeave (int num, People p);      //조퇴
+int insuranceManage (int num, People p); //보험 관리
+int changePeople (int num, People p);    //인부 교체
+int addPeople (People p);                //인부 추가
+int deletePeople (People p);             //인부 제거
+int saveFile (People p);                 //파일 저장
