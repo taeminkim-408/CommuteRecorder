6c379b2596440d1832cdf2d32ae8c78ca529509a
Somy-John <63829204+Somy-John@users.noreply.github.com>
2021-05-06 20:30:01 +0900

    Update manager.h

--- manager.h
+++ manager.h
@@ -1,11 +1,11 @@
 
 void printList (int num, People p);      //명부 출력
-int attend (int num, People p);          //출석
-int goHome (int num, People p);          //퇴근
-int night (int num, People p);           //야근
-int earlyLeave (int num, People p);      //조퇴
+int manage_attend (int num, People p);   //출결 관리 (출근, 퇴근, 야근, 조퇴)
 int insuranceManage (int num, People p); //보험 관리
 int changePeople (int num, People p);    //인부 교체
 int addPeople (People p);                //인부 추가
 int deletePeople (People p);             //인부 제거
 int saveFile (People p);                 //파일 저장
+void search_name(People p);              //이름 검색
+void search_tier(People p);              //직급 검색
+void search_attand(People p);            //출결기준 검색
