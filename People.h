1866ef81e6790f4a1dfdda6f8673a5493d2c8896
김태민 <kimtaemin@gimtaemin-ui-MacBookPro.local>
2021-05-06 20:49:16 +0900

    시작

--- People.h
+++ People.h
@@ -1,9 +1,9 @@
 
 
 typedef struct{
-char name: 
-int work:  //(1은 출근, 0은 퇴근)
-int rank; //(0은 사장, 1은 과장, 2는 부장 3,은 말단, 4는 인턴, 5는 알바)
+char name; 
+int work; //(1은 출근, 0은 퇴근)
+int tier; //(0은 사장, 1은 과장, 2는 부장 3,은 말단, 4는 인턴, 5는 알바)
 int time; //일한 시간
 }People;
 
