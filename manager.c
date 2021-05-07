720db15001eaf2c7a8611089c1de1f68ad04c9a0
taeminkim-408 <82189651+taeminkim-408@users.noreply.github.com>
2021-05-07 16:03:24 +0900

    Create manager.c

--- manager.c
+++ manager.c
@@ -1,4 +1,5 @@
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,23 +25,31 @@ int selectMenu()
 
 int addPeople(People *p)
 { // 제품을 추가하는 함수 2
-   printf("새 인부의 이름은? ");
+   printf("새 인부의 이름은? \n>> ");
    scanf(" %[^\n]", p->name);
-   printf("새 인부의 직급은? ");
+   printf("새 인부의 직급은(0:사장,1:과장,2:부장,3:말단,4:인턴,5:알바)? \n>> ");
    scanf(" %d", &p->tier);
-   printf("새 인부의 출퇴근 현황은? ");
+   printf("새 인부의 출퇴근 현황은(1:출근,2:퇴근,3:야근,4:조퇴)? \n>> ");
    scanf(" %d", &p->work);
    return 1;
 }
 
 void readPeople(People p)
 { // 하나의 제품 출력 함수
-   printf("%6s %3d %3d\n", p.name, p.tier, p.work);
+   printf(" %6s ",p.name);
+   printtier(p.tier);
+   printwork(p.work);
+   printbohum(p.bohum);
 }
+
+
+
+
+
 void printList(int count, People **p)
 { // 전체 등록된 제품 리스트 출력 1
    int i = 0;
-   printf("  이름 직급 출결 \n");
+   printf("번호 이름  직급 출결   보험  \n");
    for (i = 0; i < count; i++)
    {
       if (p[i] == NULL)
@@ -50,19 +59,75 @@ void printList(int count, People **p)
    }
 }
 
+void printtier(int tier)
+{ // 하나의 제품 출력 함수
+   if(tier==0){
+      printf("사장 ");
+   }
+   else if(tier==1){
+      printf("과장 ");
+   }
+   else if(tier==2){
+      printf("부장 ");
+   }
+   else if(tier==3){
+      printf("말단 ");
+   }
+   else if(tier==4){
+      printf("인턴 ");
+   }
+   else if(tier==5){
+      printf("알바 ");
+   }
+   else{
+      printf("X");
+   }
+}
+
+void printwork(int work){
+   if(work==1){
+      printf("출근");
+   }
+   else if(work==2){
+      printf("퇴근");
+   }
+   else if(work==3){
+      printf("야근");
+   }
+   else if(work==4){
+      printf("조퇴");
+   }
+   else{
+      printf("X");
+   }
+
+}
+
+void printbohum(int bohym){
+   if(bohym==1){
+      printf(" 보험 처리\n");
+   }
+   else if(bohym==2){
+      printf(" 정보 없음\n");
+   }
+   else{
+      printf(" 정보 없음\n");
+   }
+}
+
 int changePeople(People *p)
 { //제품 수정 3
    printf("새 인부 이름은?\n>> ");
    scanf(" %[^\n]", p->name);
-   printf("새 인부의 출퇴근 현황은?\n>> ");
+   printf("새 인부의 출퇴근 현황은?(1:출근,2:퇴근,3:야근,4:조퇴)\n>> ");
    scanf(" %d", &p->work);
-   printf("새 인부의  직급은(0은 사장, 1은 과장, 2는 부장 3,은 말단, 4는 인턴, 5는 알바)?\n>> ");
+   printf("새 인부의  직급은(0:사장,1:과장,2:부장,3:말단,4:인턴,5:알바)?\n>> ");
    scanf(" %d", &p->tier);
    return 1;
 }
 int manage_attend(People *p)
 {
-   printf("출결 관리(1출근, 2퇴근, 3야근, 4조퇴)\n>> ");
+   printf("출결 관리(1:출근, 2:퇴근, 3:야근, 4:조퇴)\n>> ");
    scanf("%d", &p->work);
    return 1;
 }
@@ -88,3 +153,4 @@ int deletePeople(People **p, int count)
    count--;
    return count;
 }
+
